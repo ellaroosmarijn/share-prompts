4064b68e97ed7411d254036b33bca95877601e72
Ella Robinson <ellaroosmarijn@gmail.com>
2023-06-11 14:14:00 +1200

    feat(client): add Provider function

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+'use client'
 
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
+import { SessionProvider } from 'next-auth/react'
+
+export default function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>
 }
-
-export default Provider
